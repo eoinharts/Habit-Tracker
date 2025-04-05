177e5727067bea5a28174e4c3b1b589d450cc882
Blaise <blaisewalsh@hotmail.com>
2025-04-05 13:03:58 +0100

    Small changes to add achievements pop-up, also removed an unused page

--- src/components/AuthenticatedRoutes.jsx
+++ src/components/AuthenticatedRoutes.jsx
@@ -48,7 +48,7 @@ export default function AuthenticatedRoutes() {
             <Route path="/components" element={<Components />} />
             <Route path="/profile" element={<ProfilePage />} />
             <Route path="/friend/:friendId" element={<FriendPage />} />
-            <Route path="/habit-home" element={<HabitHomePage />} />
+            <Route path="/achievement-test" element={<AchievementTest />} />
             <Route path="/create-habit" element={<CreateHabit />} />
           </Routes>
         </>
